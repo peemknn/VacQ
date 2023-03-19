6f39f9e7af8d734d6b80ecf852ef50fe953d1c79
Peemknn <kunanontpeem333@gmail.com>
2023-03-19 13:59:55 +0700

    Add advance query

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,88 +1,140 @@
-const Hospital = require('../models/Hospital');
+const Hospital = require('../models/Hospital')
 //@desc Get all hospitals
 //@route GET /api/v1/hospitals
 //@access Public
 exports.getHospitals = async (req, res, next) => {
-    try{
-        let query;
-        let queryStr = JSON.stringify(req.query);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match => `$$(match)`);
+    try {
+        console.log(req.query)
+        let query
+        let queryStr = JSON.stringify(req.query)
 
-        query = Hospital.find(JSON.parse(queryStr));
+        //req.query copying
+        const reqQuery = { ...req.query }
 
-        const hospitals = await query;
+        //Excluded Fields
+        const removeFields = ['select', 'sort', 'page', 'limit']
 
-        res.status(200).json({success:true,count:hospitals.length, data:hospitals});
-    }catch(err){
-        res.status(400).json({success:false});
+        //Loop over remove fields and delete them for reqQuery
+        removeFields.forEach((param) => delete reqQuery[param])
 
-    } 
-  
+        console.log(reqQuery)
+        queryStr = queryStr.replace(
+            /\b(gt|gte|lt|lte|in)\b/g,
+            (match) => `$${match}`
+        )
+
+        query = Hospital.find(JSON.parse(queryStr))
+
+        //Select fields
+        if (req.query.select) {
+            const fields = req.query.select.split(',').join(' ')
+            query = query.select(fields)
+        }
+
+        //Sort
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+
+        //Pagination
+        const page = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 25
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const total = await Hospital.countDocuments()
+        query = query.skip(startIndex).limit(limit)
+
+        //Query execution
+        const hospitals = await query
+
+        //Pagination result
+        const pagination = {}
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit,
+            }
+        }
+
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit,
+            }
+        }
+
+        res.status(200).json({
+            success: true,
+            count: hospitals.length,
+            pagination,
+            data: hospitals,
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
 
 //@desc Get single hospital
 //@route GET /api/v1/hospitals/:id
 //@access Public
-exports.getHospital = async (req, res, next) => {  
-    try{
-        const hospital = await Hospital.findById(req.params.id);
+exports.getHospital = async (req, res, next) => {
+    try {
+        const hospital = await Hospital.findById(req.params.id)
 
-        if(!hospital){
-            return res.status(400).json({success:false});
+        if (!hospital) {
+            return res.status(400).json({ success: false })
         }
-        res.status(200).json({success:true,data:hospital});
+        res.status(200).json({ success: true, data: hospital })
+    } catch (err) {
+        res.status(400).json({ success: false })
     }
-    catch(err){
-        res.status(400).json({success:false});
-    }
-};
+}
 
 //@desc Create new hospital
 //@route POST /api/v1/hospitals
 //@access Private
 exports.createHospital = async (req, res, next) => {
-    const hospital = await Hospital.create(req.body);
-    res.status(201).json({
-        success: true,
-        data: hospital
-    });
-    res.status(200).json({ success: true, msg: "Created new hospital" });
-};
-   
-
+    const hospital = await Hospital.create(req.body)
+    res.status(201).json({ success: true, data: hospital })
+}
 
 //@desc Update hospital
 //@route PUT /api/v1/hospitals/:id
 //@access Private
 exports.updateHospital = async (req, res, next) => {
-    try{
-        const hospital = await Hospital.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
-        if(!hospital){
-            return res.status(400).json({success:false});
+    try {
+        const hospital = await Hospital.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        )
+        if (!hospital) {
+            return res.status(400).json({ success: false })
         }
-        res.status(200).json({success:true, data:hospital});
-    }catch(err){
-        res.status(400).json({success:false});
+        res.status(200).json({ success: true, data: hospital })
+    } catch (err) {
+        res.status(400).json({ success: false })
     }
-   
 }
 
 //@desc Delete hospital
 //@route DELETE /api/v1/hospitals/:id
 //@access Private
 exports.deleteHospital = async (req, res, next) => {
-    try{
-        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+    try {
+        const hospital = await Hospital.findByIdAndDelete(req.params.id)
 
-        if(!hospital)
-            res.status(400).json({success:false});
+        if (!hospital) res.status(400).json({ success: false })
 
-        res.status(200).json({ success: true,data:{}});
-    } catch(err){
-        res.status(400).json({success:false});
+        res.status(200).json({ success: true, data: {} })
+    } catch (err) {
+        res.status(400).json({ success: false })
     }
-    
 }
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
+const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require("../controllers/hospitals");
 const router = express.Router();
-const {protect,authorize} = require('../middleware/auth');
+const {protect, authorize} = require("../middleware/auth");
 
-const { getHospitals, getHospital, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitals');
 
-router.route('/').get(getHospitals).post(protect,authorize('admin'), createHospital);
-router.route('/:id').get(getHospital).put(protect,authorize('admin'), updateHospital).delete(protect,authorize('admin'), deleteHospital);
-
+router.route("/").get(getHospitals).post(protect, authorize("admin"), createHospital);
+router.route("/:id").get(getHospital).put(protect, authorize("admin"), updateHospital).delete(protect, authorize("admin"), deleteHospital);
 
 module.exports = router;
