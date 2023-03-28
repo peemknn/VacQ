47406e9904e8b2b6c3ff4d66074e65b355b2ee49
Peemknn <kunanontpeem333@gmail.com>
2023-03-29 01:05:37 +0700

    add frontend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 
 // Load env vars
@@ -15,6 +16,7 @@ const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
 
 const app = express();
+app.use(cors());
 
 //Body parser
 app.use(express.json());
